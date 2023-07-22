fcf6f1d3a7ebfadb22b037fbe0c5bcb4e06f3008
benfyne <133433515+benfyne@users.noreply.github.com>
2023-07-22 22:55:03 +0100

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -2,9 +2,9 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the argument
+ * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
- * @list: list of argument.
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
