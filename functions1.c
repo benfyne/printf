f61690d550d9c1eb2e92ea9618e23be62d154edd
FaithDakoru <133269309+FaithDakoru@users.noreply.github.com>
2023-07-23 02:53:13 +0600

    Update functions1.c
    Team project

--- functions1.c
+++ functions1.c
@@ -40,7 +40,7 @@ int print_unsigned(va_list types, char buffer[],
  * print_octal - Prints an unsigned number in octal notation
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculate active flags
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
